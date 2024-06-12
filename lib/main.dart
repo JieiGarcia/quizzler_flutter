768bbeb54821e7b89fe8ea809969d1c127aa3469
JieiGarcia <jiei.aca@gmail.com>
2024-06-12 12:16:48 +0900

    created a class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler_flutter/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -39,6 +40,11 @@ class _QuizPageState extends State<QuizPage> {
 
   List<bool> answers = [false, true, false];
 
+  Question q1 = Question(
+    q: 'You can lead a cow down stairs but not up stairs.',
+    a: false,
+  );
+
   int qNum = 0;
 
   void addNum() {
@@ -73,12 +79,12 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
                 questions[qNum],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
